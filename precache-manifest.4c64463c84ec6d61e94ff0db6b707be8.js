61a0d16ad451e46be548b6a66ab03c6e13a45b82
Habib Afolabi <habibwritescode@gmail.com>
2020-06-13 20:21:31 +0100

    Updates

---
+++ precache-manifest.4c64463c84ec6d61e94ff0db6b707be8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854a9b85d0416ba8184b8107f20d478b",
+    "url": "/iRobot/index.html"
+  },
+  {
+    "revision": "3ca4a3f8e20a5ea56c77",
+    "url": "/iRobot/static/css/main.3074c649.chunk.css"
+  },
+  {
+    "revision": "0f05fbb2bb76b0455248",
+    "url": "/iRobot/static/js/2.62cea425.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iRobot/static/js/2.62cea425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca4a3f8e20a5ea56c77",
+    "url": "/iRobot/static/js/main.cb725b2b.chunk.js"
+  },
+  {
+    "revision": "dc2222a55a1e254c57fc",
+    "url": "/iRobot/static/js/runtime-main.c1addb97.js"
+  }
+]);
